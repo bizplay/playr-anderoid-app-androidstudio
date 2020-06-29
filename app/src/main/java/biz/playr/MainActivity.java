4ee286bd7c92e93dcc3eea5d734e6550f73af4d8
Bob Groeneveld <bob.groeneveld@gmail.com>
2020-06-29 17:02:52 +0200

    Add extra 'full screen' combination of setting

--- app/src/main/java/biz/playr/MainActivity.java
+++ app/src/main/java/biz/playr/MainActivity.java
@@ -159,6 +159,7 @@ public class MainActivity extends Activity implements IServiceCallbacks {
 
 		// Set up looks of the view
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		View decorView = getWindow().getDecorView();
 		decorView
@@ -722,10 +723,10 @@ public class MainActivity extends Activity implements IServiceCallbacks {
 			// a general rule, you should design your app to hide the status bar whenever you
 			// hide the navigation bar.
 			int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-					| View.SYSTEM_UI_FLAG_FULLSCREEN
-					| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-					| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-					| View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+							| View.SYSTEM_UI_FLAG_FULLSCREEN
+							| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+							| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+							| View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
 			// Enables regular immersive mode.
 			// For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
 			// Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
