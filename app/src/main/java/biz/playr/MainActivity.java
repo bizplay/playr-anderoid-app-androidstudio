cf0f053428cbf65184bbfc1c2e07ea8a7bc1ca6c
Bob Groeneveld <bob.groeneveld@gmail.com>
2021-09-16 14:24:39 +0200

    First step to respond to low memory

--- app/src/main/java/biz/playr/MainActivity.java
+++ app/src/main/java/biz/playr/MainActivity.java
@@ -3,7 +3,9 @@ package biz.playr;
 import java.util.UUID;
 
 import android.Manifest;
+import android.app.ActivityManager;
 import android.app.UiModeManager;
+import android.content.ComponentCallbacks2;
 import android.content.ComponentName;
 import android.content.ServiceConnection;
 import android.content.res.Configuration;
@@ -50,7 +52,7 @@ import androidx.browser.customtabs.TrustedWebUtils;
 import androidx.core.app.ActivityCompat;
 import androidx.webkit.WebViewCompat;
 
-public class MainActivity extends Activity implements IServiceCallbacks {
+public class MainActivity extends Activity implements IServiceCallbacks, ComponentCallbacks2 {
 	private WebView webView = null;
 	private static final String className = "biz.playr.MainActivity";
 	private CheckRestartService checkRestartService;
@@ -473,6 +475,55 @@ public class MainActivity extends Activity implements IServiceCallbacks {
 		Log.i(className, "restartDelayed: end");
 	}
 
+	// implement the ComponentCallbacks2 interface
+	/**
+	 * Release memory when the UI becomes hidden or when system resources become low.
+	 * @param level the memory-related event that was raised.
+	 */
+	public void onTrimMemory(int level) {
+		Log.i(className, "onTrimMemory");
+
+		Log.w(className, "onTrimMemory - level: " + level);
+		// Determine which lifecycle or system event was raised.
+		switch (level) {
+			case ComponentCallbacks2.TRIM_MEMORY_UI_HIDDEN:
+                // Release any UI objects that currently hold memory.
+                // The user interface has moved to the background.
+				// RELOAD WEBVIEW
+				break;
+			case ComponentCallbacks2.TRIM_MEMORY_RUNNING_MODERATE:
+			case ComponentCallbacks2.TRIM_MEMORY_RUNNING_LOW:
+			case ComponentCallbacks2.TRIM_MEMORY_RUNNING_CRITICAL:
+                // Release any memory that your app doesn't need to run.
+				//
+                // The device is running low on memory while the app is running.
+                // The event raised indicates the severity of the memory-related event.
+                // If the event is TRIM_MEMORY_RUNNING_CRITICAL, then the system will
+                // begin killing background processes.
+				// RELOAD WEBVIEW
+				break;
+			case ComponentCallbacks2.TRIM_MEMORY_BACKGROUND:
+			case ComponentCallbacks2.TRIM_MEMORY_MODERATE:
+			case ComponentCallbacks2.TRIM_MEMORY_COMPLETE:
+                // Release as much memory as the process can.
+				//
+                // The app is on the LRU list and the system is running low on memory.
+                // The event raised indicates where the app sits within the LRU list.
+                // If the event is TRIM_MEMORY_COMPLETE, the process will be one of
+                // the first to be terminated.
+				// RESTART THE APP
+				break;
+			default:
+                // Release any non-critical data structures.
+				//
+                // The app received an unrecognized memory level value
+                // from the system. Treat this as a generic low-memory message.
+				// RELOAD WEBVIEW
+				break;
+		}
+	}
+	// end of implementation ComponentCallbacks2
+
 	// implement the IServiceCallbacks interface
 	public void restartActivityWithDelay() {
 		this.restartActivity();
@@ -775,6 +826,14 @@ public class MainActivity extends Activity implements IServiceCallbacks {
 	/*
 	 * PRIVATE methods
 	 */
+	// Get a MemoryInfo object for the device's current memory status.
+	private ActivityManager.MemoryInfo getAvailableMemory() {
+		ActivityManager activityManager = (ActivityManager) this.getSystemService(ACTIVITY_SERVICE);
+		ActivityManager.MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
+		activityManager.getMemoryInfo(memoryInfo);
+		return memoryInfo;
+	}
+
 	private String getStoredPlayerId() {
 		SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
 		return sharedPreferences.getString(getString(R.string.player_id_store), "");
