18a509dd26e590f6d2887c4cc323e47cb4627f97
Bob Groeneveld <bob.groeneveld@gmail.com>
2022-12-08 12:53:13 +0100

    Incorporate currenlty inactive code to start the main activity from the bootup receiver (need to test if necessary since Android Q)

--- app/src/main/java/biz/playr/BootUpReceiver.java
+++ app/src/main/java/biz/playr/BootUpReceiver.java
@@ -1,12 +1,16 @@
 package biz.playr;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 
 public class BootUpReceiver extends BroadcastReceiver{
-	private static final String className = "bz.playr.BootUpReceiver";
+	private static final String className = "biz.playr.BootUpReceive";
+	private static final int identifier = 12345;
 	/* this class is used to force a restart of the MainActivity after the device is rebooted
 	 * for instance when Android has updated
 	 * see the <receiver> section of the AndroidManifest file
@@ -28,6 +32,33 @@ public class BootUpReceiver extends BroadcastReceiver{
 			// activityIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
 			//		| Intent.FLAG_ACTIVITY_CLEAR_TASK
 			//		| Intent.FLAG_ACTIVITY_NEW_TASK);
+
+//			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+//				Intent activityIntent = new Intent(context, MainActivity.class);
+//				activityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+//						| Intent.FLAG_ACTIVITY_CLEAR_TASK
+//						| Intent.FLAG_ACTIVITY_NEW_TASK);
+//				activityIntent.setAction(Intent.ACTION_MAIN);
+//				activityIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+//				// As of S/31 FLAG_IMMUTABLE/FLAG_MUTABLE is required
+//				PendingIntent localPendingIntent = PendingIntent.getActivity(context, 0, activityIntent, PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_IMMUTABLE);
+//				try {
+//					localPendingIntent.send(context, identifier, activityIntent);
+//				} catch (PendingIntent.CanceledException e) {
+//					Log.e(className, ".onReceive: PendingIntent cancelled: " + e.getMessage());
+//				}
+//				Log.i(className, ".onReceive: called PendingIntent.send()");
+//			} else {
+//				Intent activityIntent = new Intent(context, MainActivity.class);
+//				activityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+//						| Intent.FLAG_ACTIVITY_CLEAR_TASK
+//						| Intent.FLAG_ACTIVITY_NEW_TASK);
+//				activityIntent.setAction(Intent.ACTION_MAIN);
+//				activityIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+//				context.startActivity(activityIntent);
+//				Log.i(className, ".onReceive: called context.startActivity()");
+//			}
+
 			Intent activityIntent = new Intent(context, MainActivity.class);
 			activityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
 					| Intent.FLAG_ACTIVITY_CLEAR_TASK
