15f2cfcc1c0e21a1695452ea7fba5b8c77d1ba7f
Bob Groeneveld <bob.groeneveld@gmail.com>
2025-04-22 22:17:17 +0200

    Improve https handling for devices running Android 9 or higher

--- app/src/main/java/biz/playr/MainActivity.java
+++ app/src/main/java/biz/playr/MainActivity.java
@@ -1257,7 +1257,13 @@ public class MainActivity extends Activity implements IServiceCallbacks {
 
 	private String httpsRequired() {
 		if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.M) {
+			// For Android version lower than 6 API level 23, https is not required
 			return "no";
+		} if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) {
+			// For Android versions greater than or equal 9 API level 28, only https is allowed
+			// Since isCleartextTrafficPermitted() returns true on some devices running
+			// Android 9 or higher, no check is performed
+			return "yes";
 		} else {
 			return NetworkSecurityPolicy.getInstance().isCleartextTrafficPermitted() ? "no" : "yes";
 		}
